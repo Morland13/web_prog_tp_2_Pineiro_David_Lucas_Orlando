2f33b8d260434e5b76d508bd9e39fbc780e59dbd
David Ezequiel Piñeiro <101911710+DeivPineiro@users.noreply.github.com>
2023-06-04 15:53:31 -0300

    Push!

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PLOD StarWars</title>
+</head>
+<body>
+    
+<nav class="navbar bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">
+      <img src="img/css_resources/backNav.png" alt="navLogo" width="30" height="24" class="d-inline-block align-text-top">
+      Bootstrap
+    </a>
+  </div>
+</nav>
+
+
+</body>
+</html>
