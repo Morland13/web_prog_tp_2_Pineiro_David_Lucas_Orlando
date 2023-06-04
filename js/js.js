19446be115875d8166beefd7426c47e90ecc855f
David Ezequiel Piñeiro <101911710+DeivPineiro@users.noreply.github.com>
2023-06-04 18:12:18 -0300

    Se cargar fetch donde trae las categorias

---
+++ js/js.js
@@ -0,0 +1,91 @@
+const elementoCategorias = document.getElementById('divCategorias')
+
+
+//EJEMPLO DE COMO RECORRER UN JSON UTIL PARA LA APP
+
+
+/*
+// JSON con distintos valores para utilizar en la demo
+var json = {'valor1': 1, 'valor2': [1, 2, 3, 4], 'valor3': '3'};
+     
+// Obteniendo todas las claves del JSON
+for (var clave in json){
+  // Controlando que json realmente tenga esa propiedad
+  if (json.hasOwnProperty(clave)) {
+    // Mostrando en pantalla la clave junto a su valor
+    alert("La clave es " + clave+ " y el valor es " + json[clave]);
+  }
+}
+*/
+
+elementoCategorias.innerHTML = `<div class="container text-center mt-5"><p>Si esperar tu puedes, categorias ver pronto...</p></div>`;
+
+fetch(`https://swapi.dev/api/`)
+    .then((resp) => {
+
+
+        console.log(resp);
+        elementoCategorias.innerHTML = `<p class="container text-center mt-5">Conectar estamos, tu fuerza al monitor debes...</p>`;
+        return resp.json();
+
+
+    })
+    .then(function (json) {
+
+        console.log(json);
+
+        elementoCategorias.innerHTML = ``;
+        
+        //href="index.php?sec=${i} 
+
+        for (var i in json){
+
+            if(json.hasOwnProperty(i)){
+
+                elementoCategorias.innerHTML = elementoCategorias.innerHTML + ` 
+                
+                
+                <div class="container row">
+
+                <div class="col-5 m-2">
+                
+                <img class="img-fluid" src="img/categories/${i}.jpg" alt="categoria">
+                <p class="text-center">${i}</p>
+                
+                </div>           
+                </div>`;
+
+            }
+
+
+        }
+       
+
+
+
+
+
+
+
+    })
+
+
+
+
+
+
+
+
+
+
+
+cargarCategorias = () => {
+
+
+
+
+
+
+
+
+}
